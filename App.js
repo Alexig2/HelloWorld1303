31de4a397594bcd6c819ef9d36973ce05260c62e
Alexig2 <alex4342on@gmail.com>
2024-03-13 14:28:59 -0400

    Atualização 1

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+
+function Header(){
+  return(
+    <View style={{backgroundColor:"grey"}}>
+      <Text style={{textAlign:"center"}}>
+        Componente Header
+      </Text>
+    </View>
+  )
+}
+
+function Footer(){
+  return(
+    <View>
+      <Text style={styles.Footer}>
+        PDM1 ₢ 2024
+      </Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+
+      <Text style = {styles.saudacao}>
+        Olá, eu sou...
+      </Text>
+      <TextInput style = {styles.TextInput} defaultValue = "Digite seu nome"/>
+
+      <Footer/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,5 +43,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Footer: {
+    justifyContent: 'center',
+  },
+  saudacao: {
+    color: 'green',
+  },
+  TextInput:{
+    height: 30,
+    width: 200,
+    borderColor: 'blue',
+    borderWidith: 1
   },
 });
